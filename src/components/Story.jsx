aa27dbba44c19c6013365efd79a3f644723f2a46
Belal Hussien <bh255890@gmail.com>
2025-01-26 08:15:49 +0200

    fix story comp3

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -36,7 +36,7 @@ const ExperienceCard = ({ experience }) => {
       <div>
         <h3 className='text-white text-[14px] sm:text-[20px] font-bold'>{experience.title}</h3>
         <p
-          className='text-secondary text-[10px] sm:text-[14px] font-semibold'
+          className='text-secondary text-[10px] sm:text-[14px] font-semibold overflow-hidden'
           style={{ margin: 0 }}
         >
         </p>
@@ -46,7 +46,7 @@ const ExperienceCard = ({ experience }) => {
         {experience.points.map((point, index) => (
           <li
             key={`experience-point-${index}`}
-            className='text-white-100 text-[10px] sm:text-[14px] pl-1 tracking-wider overflow-hidden'
+            className='text-white-100 text-[10px] sm:text-[14px] pl-1 tracking-wider'
           >
             {point}
           </li>
